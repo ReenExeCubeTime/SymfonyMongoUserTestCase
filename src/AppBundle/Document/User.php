70dc6476e7f93c89a73b9b8b3ef211f597637f20
ReenExe <ipourri@gmail.com>
2016-01-30 17:54:25 +0200

    progress

--- src/AppBundle/Document/User.php
+++ src/AppBundle/Document/User.php
@@ -14,4 +14,41 @@ class User extends BaseUser
      * @MongoDB\Id(strategy="auto")
      */
     protected $id;
+
+    /**
+     * @MongoDB\String
+     */
+    protected $name;
+
+    /**
+     * Get id
+     *
+     * @return id $id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return self
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string $name
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
 }
