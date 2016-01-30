e63c94e659930ff39ee656e95c75528e9ee8012a
ReenExe <ipourri@gmail.com>
2016-01-30 18:18:00 +0200

    for see fill roles

--- src/AppBundle/Controller/Api/AdminUserController.php
+++ src/AppBundle/Controller/Api/AdminUserController.php
@@ -19,7 +19,7 @@ class AdminUserController extends Controller
 
         $form->submit($request);
 
-        $user->setRoles(['ROLE_USER']);
+        $user->setRoles(['ROLE_DEVELOPER']);
 
         $this->get('em.user')->add($user);
 
