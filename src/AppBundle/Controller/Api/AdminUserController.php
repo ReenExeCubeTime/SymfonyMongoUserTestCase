1acbd79345c28d1f2884dc821573c053f28fdc1b
ReenExe <ipourri@gmail.com>
2016-01-30 17:28:07 +0200

    progress

--- src/AppBundle/Controller/Api/AdminUserController.php
+++ src/AppBundle/Controller/Api/AdminUserController.php
@@ -2,13 +2,28 @@
 
 namespace AppBundle\Controller\Api;
 
+use AppBundle\Document\User;
+use AppBundle\Type\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 class AdminUserController extends Controller
 {
-    public function addAction()
+    public function addAction(Request $request)
     {
+        $user = new User();
+
+        $form = $this->createForm(new UserType(), $user);
+
+        $form->submit($request);
+
+        /* @var $dm DocumentManager */
+        $dm = $this->get('doctrine_mongodb')->getManager();
+
+        $dm->persist($user);
+        $dm->flush();
+
         return new JsonResponse([
             'success' => true
         ]);
