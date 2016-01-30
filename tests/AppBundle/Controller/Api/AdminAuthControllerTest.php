e087d199385db8d7b9f33240c2f03740542130e6
ReenExe <ipourri@gmail.com>
2016-01-30 18:52:11 +0200

    progress

--- tests/AppBundle/Controller/Api/AdminAuthControllerTest.php
+++ tests/AppBundle/Controller/Api/AdminAuthControllerTest.php
@@ -6,13 +6,21 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class AdminAuthControllerTest extends WebTestCase
 {
+    private $client;
+
+    protected function setUp()
+    {
+        $this->client = static::createClient();
+
+        /* @var $dm ManagerRegistry */
+        $dm = static::$kernel->getContainer()->get('doctrine_mongodb');
+    }
+
     public function testIndex()
     {
-        $client = static::createClient();
+        $this->client->request('POST', '/api/auth');
 
-        $client->request('POST', '/api/auth');
-
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertSame($client->getResponse()->getContent(), '[]');
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+        $this->assertSame($this->client->getResponse()->getContent(), '[]');
     }
 }
