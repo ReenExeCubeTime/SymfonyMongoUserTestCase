6aac5f44e51587330838ad5ba6bff98910186b35
ReenExe <ipourri@gmail.com>
2016-01-30 17:13:25 +0200

    copy-past from https://github.com/ReenExe/BookTranslateSlicer

---
+++ tests/AppBundle/Type/UserType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AppBundle\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('id');
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Document\User',
+        ));
+    }
+
+    public function getName()
+    {
+        return 'user';
+    }
+}
