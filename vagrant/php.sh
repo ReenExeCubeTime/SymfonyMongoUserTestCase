638a322048092bbbbe23c5c799e2310f349b4822
ReenExe <ipourri@gmail.com>
2016-01-31 16:19:29 +0200

    delete first try

--- vagrant/php.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-sudo aptitude install -q -y -f php5-cli php5-curl
-
-sudo apt-get install -y php-pear php5-dev
-sudo apt-get install libsasl2-dev
-sudo pecl install mongo
-# You should add "extension=mongo.so" to php.ini
